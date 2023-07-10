8bc2632e55203340ed2fd185663693739c49b772
Václav Blažej <vaclav.blazej@warwick.ac.uk>
2023-07-10 10:58:47 +0100

    Update for Ubuntu 22.04

--- lua/blazeva1/key_remaps.lua
+++ lua/blazeva1/key_remaps.lua
@@ -7,14 +7,14 @@
 -- o: Operator-pending.
 -- '': = n + v + o.
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- vim.keymap.set("v", "<", "mz<gv`z4<Left>")
 -- vim.keymap.set("v", ">", "mz>gv`z4<Right>")
 
-vim.keymap.set("n", "J", "mzJ`z")
+-- vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
